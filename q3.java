5f6ca344a429f3338a98b34b6181c3bbefbc894f
vc0466 <vc0466@srmist.edu.in>
2026-03-02 14:00:49 +0530

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,10 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double conversionFactor = 1.6;  // 1 km = 1.6 miles (as given)
+
+        double miles = kilometers * conversionFactor;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
