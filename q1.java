8fa92db82e0f446b5d8fd9f2a328dc9527562276
vc0466 <vc0466@srmist.edu.in>
2026-03-02 23:51:17 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,26 +1,36 @@
-// q1.java
+// Basic Calculator Program
+
 import java.util.Scanner;
 
 public class q1 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-
-        double number1, number2;
-
+        
         System.out.print("Enter first number: ");
-        number1 = sc.nextDouble();
-
+        float number1 = sc.nextFloat();
+        
         System.out.print("Enter second number: ");
-        number2 = sc.nextDouble();
+        float number2 = sc.nextFloat();
 
-        double addition = number1 + number2;
-        double subtraction = number1 - number2;
-        double multiplication = number1 * number2;
-        double division = number1 / number2;
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
 
-        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+        float division;
+        if (number2 != 0) {
+            division = number1 / number2;
+        } else {
+            System.out.println("Division by zero is not allowed.");
+            sc.close();
+            return;
+        }
+        
+        System.out.println("\nThe addition, subtraction, multiplication, and division value of 2 numbers "
                 + number1 + " and " + number2 + " is "
                 + addition + ", " + subtraction + ", "
                 + multiplication + ", and " + division);
+        
+        sc.close();
     }
 }
